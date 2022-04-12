e92303cca3c037bd33353449e91ba7d082114840
Oskar Schilling <80629512+GROMoOS@users.noreply.github.com>
2022-04-12 21:40:58 +0200

    Implement observer for a driver change event.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/gui/DriverManagerDriverChangeObserver.java
@@ -0,0 +1,15 @@
+package edu.kis.powp.jobs2d.drivers.gui;
+
+import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.observer.Subscriber;
+
+public class DriverManagerDriverChangeObserver implements Subscriber {
+
+	public DriverManagerDriverChangeObserver() {
+	}
+
+	@Override
+	public void update() {
+		DriverFeature.updateDriverInfo();
+	}
+}
