721c83410ba3c4cbd128d4da5d739a8043e54042
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-04-13 10:52:14 +0200

    modified ComplexCommandBuilder.

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandBuilder.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandBuilder.java
@@ -1,16 +1,26 @@
 package edu.kis.powp.jobs2d.command;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ComplexCommandBuilder {
-    private final ArrayList<DriverCommand> commandList = new ArrayList<>();
+    private final List<DriverCommand> commandList = new ArrayList<>();
 
-    public ComplexCommandBuilder addCommand(DriverCommand command) {
+    public ComplexCommandBuilder addCommand(final DriverCommand command) {
         commandList.add(command);
         return this;
     }
 
+    public ComplexCommandBuilder addOperateTo(int posX, int posY) {
+        return addCommand(new OperateToCommand(posX, posY));
+    }
+
+    public ComplexCommandBuilder addSetPosition(int posX, int posY) {
+        return addCommand(new SetPositionCommand(posX, posY));
+    }
+
     public ComplexCommand build() {
+        commandList.clear();
         return new ComplexCommand(commandList);
     }
 }
