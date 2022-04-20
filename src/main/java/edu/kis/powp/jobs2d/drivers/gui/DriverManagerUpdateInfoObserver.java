65598b1b9510e53321011f28b490825aa93ccf51
backs <26674182+Backss4@users.noreply.github.com>
2022-04-20 10:27:02 +0200

    Created new class for updating application gui driver's info

--- src/main/java/edu/kis/powp/jobs2d/drivers/gui/DriverManagerUpdateInfoObserver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/gui/DriverManagerUpdateInfoObserver.java
@@ -1,2 +1,14 @@
-package edu.kis.powp.jobs2d.drivers.gui;public class DriverManagerUpdateInfoObserver {
+package edu.kis.powp.jobs2d.drivers.gui;
+
+import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.observer.Subscriber;
+
+public class DriverManagerUpdateInfoObserver implements Subscriber {
+
+	@Override
+	public void update() {
+		DriverFeature.updateDriverInfo();
+	}
+
 }
