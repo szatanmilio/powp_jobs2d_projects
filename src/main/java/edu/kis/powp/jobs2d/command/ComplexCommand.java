0fbfea851d5e383f8aba1e4b3302e11e31a90bba
piterm11 <piteralke@gmail.com>
2022-04-20 10:33:33 +0200

    Added constructor with list argument

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -6,23 +6,25 @@ import java.util.List;
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.command.DriverCommand;
 
-public class ComplexCommand implements DriverCommand{
+public class ComplexCommand implements DriverCommand {
 
 	private List<DriverCommand> commands;
 
-	public ComplexCommand()
-	{
+	public ComplexCommand() {
 		this.commands = new ArrayList<>();
 	}
-	public void appendCommand(DriverCommand command)
-	{
+
+	public ComplexCommand(List<DriverCommand> commandList) {
+		this.commands = commandList;
+	}
+
+	public void appendCommand(DriverCommand command) {
 		this.commands.add(command);
 	}
 
 	@Override
 	public void execute(Job2dDriver job2dDriver) {
-		for(DriverCommand command: commands)
-		{
+		for (DriverCommand command : commands) {
 			command.execute(job2dDriver);
 		}
 	}
