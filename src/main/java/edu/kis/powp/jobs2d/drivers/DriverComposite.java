aa9420b8b2a621c73725354cf0ea72d46fc4ca15
rafiti12 <rafiti12@o2.pl>
2022-05-18 14:18:50 +0200

    DriverComposite - deleted unnecessary methods - changed file formatting to tabs from spaces

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
@@ -6,43 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DriverComposite implements Job2dDriver {
-    private List<Job2dDriver> driverList;
+	private List<Job2dDriver> driverList;
 
-    public DriverComposite(List<Job2dDriver> driverList) {
-        this.driverList = driverList;
-    }
+	public DriverComposite(List<Job2dDriver> driverList) {
+		this.driverList = driverList;
+	}
 
-    public DriverComposite(){
-        List<Job2dDriver> driverList = new ArrayList<Job2dDriver>();
-    }
+	public DriverComposite(){
+		List<Job2dDriver> driverList = new ArrayList<Job2dDriver>();
+	}
 
-    public void addDriver(Job2dDriver driver){
-        driverList.add(driver);
-    }
+	public void addDriver(Job2dDriver driver){
+		driverList.add(driver);
+	}
 
-    public void removeDriver(Job2dDriver driver){
-        driverList.remove(driver);
-    }
+	public void removeDriver(Job2dDriver driver){
+		driverList.remove(driver);
+	}
 
-    @Override
-    public void setPosition(int x, int y) {
-        for (Job2dDriver job2dDriver : driverList) {
-            job2dDriver.setPosition(x, y);
-        }
-    }
+	@Override
+	public void setPosition(int x, int y) {
+		for (Job2dDriver job2dDriver : driverList) {
+			job2dDriver.setPosition(x, y);
+		}
+	}
 
-    @Override
-    public void operateTo(int x, int y) {
-        for (Job2dDriver job2dDriver : driverList) {
-            job2dDriver.operateTo(x, y);
-        }
-    }
-
-    public List<Job2dDriver> getDriverList() {
-        return driverList;
-    }
-
-    public void setDriverList(List<Job2dDriver> driverList) {
-        this.driverList = driverList;
-    }
+	@Override
+	public void operateTo(int x, int y) {
+		for (Job2dDriver job2dDriver : driverList) {
+			job2dDriver.operateTo(x, y);
+		}
+	}
 }
