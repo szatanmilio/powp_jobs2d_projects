075ec0b643afc85e00745faa109543a96b60d57c
Karol Profic <karolprofic@protonmail.com>
2022-05-19 01:01:05 +0200

    Zmiany

--- src/main/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,11 +2,19 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.command.ComplexCommand;
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.OperateToCommand;
+import edu.kis.powp.jobs2d.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.factories.ComplexCommandFactory;
 import edu.kis.powp.jobs2d.factories.ComplexCommandFactory.TestShape;
+import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -23,7 +31,8 @@ public class SelectTestFigureOptionListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Job2dDriver driver = driverManager.getCurrentDriver();
-		ComplexCommandFactory factory = new ComplexCommandFactory();
+		List<DriverCommand> commands = new ArrayList<DriverCommand>();
+		ComplexCommand complexCommand = new ComplexCommand(commands);
 		switch (this.testShape) {
 			case FIGURE1:
 				FiguresJoe.figureScript1(driver);
@@ -32,10 +41,25 @@ public class SelectTestFigureOptionListener implements ActionListener {
 				FiguresJoe.figureScript2(driver);
 				break;
 			case RECTANGLE:
-				factory.drawRectangle(100,100,100,100).execute(driver);
+				int x = 100;
+				int y = 100;
+				int width = 100;
+				int height = 100;
+				commands.add(new SetPositionCommand(x, y));
+				commands.add(new OperateToCommand(x+width, y));
+				commands.add(new OperateToCommand(x+width, y+height));
+				commands.add(new OperateToCommand(x, y+height));
+				commands.add(new OperateToCommand(x, y));
+				complexCommand.execute(driver);
 				break;
 			case STAR:
-				factory.drawStar().execute(driver);
+				commands.add(new SetPositionCommand(60, 20));
+				commands.add(new OperateToCommand(100, 110));
+				commands.add(new OperateToCommand(10, 50));
+				commands.add(new OperateToCommand(110, 50));
+				commands.add(new OperateToCommand(20, 110));
+				commands.add(new OperateToCommand(60, 20));
+				complexCommand.execute(driver);
 				break;
 		}
 	}
